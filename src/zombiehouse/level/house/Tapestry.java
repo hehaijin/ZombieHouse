4f6c5bff8435195f6aaba2a53d1ef58e5968bab2
hehaijin <hehaijin1981@gmai.com>
2016-10-15 14:29:40 -0600

    a Tapestry class

---
+++ src/zombiehouse/level/house/Tapestry.java
@@ -0,0 +1,43 @@
+package zombiehouse.level.house;
+
+import javafx.scene.paint.Color;
+import zombiehouse.common.LevelVar;
+
+public class Tapestry extends Wall
+{
+
+  /**
+   * constructor
+   * @param xCor
+   * @param yCor
+   * @param zone
+   */
+  public Tapestry(int xCor, int yCor, int zone)
+  {
+    super(xCor, yCor, zone);
+  }
+
+  /**
+   * For printHouse method in Level class
+   * @return
+   */
+  public char getChar()
+  {
+    return '^';
+  }
+
+
+  /**
+   *
+   * @return
+   */
+  public Color getColor()
+  {
+    return null;
+  }
+
+
+
+
+}
+
